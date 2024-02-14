6f85b8bc76f06f2142e390c51f97d6e975109f91
ncar285 <carlier.nicholas@gmail.com>
2024-02-14 19:35:54 +1300

    update invite friends table

--- app/ui/dashboard/invite-friends.tsx
+++ app/ui/dashboard/invite-friends.tsx
@@ -2,11 +2,11 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-import { fetchFriends, fetchLatestInvoices } from '@/app/lib/data';
+// import { LatestInvoice } from '@/app/lib/definitions';
+import { fetchFriends } from '@/app/lib/data';
 
 export default async function InviteFriends() {
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
   const friends = await fetchFriends();
 
   console.log("user's friends: ", friends)
@@ -18,10 +18,10 @@ export default async function InviteFriends() {
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
 
         <div className="bg-white px-6">
-          {latestInvoices.map((invoice, i) => {
+          {friends.map((friend, i) => {
             return (
               <div
-                key={invoice.id}
+                key={friend.id}
                 className={clsx(
                   'flex flex-row items-center justify-between py-4',
                   {
@@ -31,34 +31,37 @@ export default async function InviteFriends() {
               >
                 <div className="flex items-center">
                   <Image
-                    src={invoice.image_url}
-                    alt={`${invoice.name}'s profile picture`}
+                    src={friend.image_url || '/users/default-profile-image.png'}
+                    alt={`${friend.name}'s profile picture`}
                     className="mr-4 rounded-full"
                     width={32}
                     height={32}
                   />
                   <div className="min-w-0">
                     <p className="truncate text-sm font-semibold md:text-base">
-                      {invoice.name}
+                      {friend.name}
                     </p>
-                    <p className="hidden text-sm text-gray-500 sm:block">
+                    {/* <p className="hidden text-sm text-gray-500 sm:block">
                       {invoice.email}
-                    </p>
+                    </p> */}
                   </div>
                 </div>
                 <p
                   className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
                 >
-                  {invoice.amount}
+                  {/* {invoice.amount} */}
+                  <button>
+                    send invite
+                  </button>
                 </p>
               </div>
             );
           })}
         </div>
-        <div className="flex items-center pb-2 pt-6">
+        {/* <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
-        </div>
+        </div> */}
       </div>
     </div>
   );
