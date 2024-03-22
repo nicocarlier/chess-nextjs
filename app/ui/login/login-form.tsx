da089c4870ba700d8374df8392d26a881d572ac5
Nico Carlier <carlier.nicholas@gmail.com>
2024-03-22 13:46:29 +1300

    temp remove oauth

--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -16,9 +16,9 @@ export default function LoginForm() {
         Sign In
       </h1>
 
-      <GoogleSignInButton/>
+      {/* <GoogleSignInButton/>
 
-      <GitHubSignInButton/>
+      <GitHubSignInButton/> */}
 
       <EmailSignInButton/>
 
