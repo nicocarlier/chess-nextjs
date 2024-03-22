58a84c961c2ab2b764b1e771effd8c62a18a2400
Nico Carlier <carlier.nicholas@gmail.com>
2024-03-22 23:45:45 +1300

    .

--- app/ui/login/email/EmailSignInButton.tsx
+++ app/ui/login/email/EmailSignInButton.tsx
@@ -1,19 +1,13 @@
 import styles from './EmailSignInButton.module.css';
-import { signIn } from "next-auth/react";
 import {
   EnvelopeIcon,
 } from '@heroicons/react/24/outline';
-import { getAbsoluteUrl } from '@/app/lib/utils';
-import { useEffect } from 'react';
 import Link from 'next/link';
 
-const EMAIL_AUTH_URL = "/login/email-auth"
-
 export function EmailSignInButton() {
-  const url = getAbsoluteUrl(EMAIL_AUTH_URL)
 
   return (
-    <Link href={url} className={styles.button}>
+    <Link href="/login/email-auth" className={styles.button}>
       <span className={styles.inputIconContainer}>
         <EnvelopeIcon className={styles.inputIcon} />
       </span>
